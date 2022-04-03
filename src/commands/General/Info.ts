2964ed6eb334f8030d9f50052248ffc8baca866c
Ray <89797110+Whatup364@users.noreply.github.com>
2022-04-04 00:09:16 +0530

    Update Info.ts

--- src/commands/General/Info.ts
+++ src/commands/General/Info.ts
@@ -1,63 +1,47 @@
-//** @format */
-
-//** @format */
-
-import { MessageType, Mimetype } from "@adiwajshing/baileys/lib/WAConnection";
+import { MessageType, Mimetype } from "@adiwajshing/baileys";
 import MessageHandler from "../../Handlers/MessageHandler";
 import BaseCommand from "../../lib/BaseCommand";
+import request from "../../lib/request";
 import WAClient from "../../lib/WAClient";
-import * as typings from "../../typings";
+import { ISimplifiedMessage } from "../../typings";
 
 export default class Command extends BaseCommand {
 	constructor(client: WAClient, handler: MessageHandler) {
 		super(client, handler, {
 			command: "info",
-			description: "Will display the info of the bot",
-			category: "general",
+			description: "Will display the info the bot",
+			category: "utils",
 			usage: `${client.config.prefix}info`,
 			baseXp: 0,
 		});
 	}
 
-	run = async (M: typings.ISimplifiedMessage): Promise<void> => {
-		//eslint-disable @typescript-eslint/no-explicit-any/
-        const chats: any = this.client.chats		
+	run = async (M: ISimplifiedMessage): Promise<void> => {
+		/*eslint-disable @typescript-eslint/no-explicit-any*/
+                const users = await this.client.DB.user.countDocuments();
+                const uban = await this.client.DB.user.countDocuments({ban: true});
+		const chats: any = this.client.chats
 			.all()
-			.filter((v) => !v.read_only && !v.archive)
+			.filter((v) => !v.jid.endsWith && !v.archive)
 			.map((v) => v.jid)
-			.map((jids) => (jids.includes("g.us") ? jids : null))
+			.map((jids) => (jids.includes("g.us") ? jids : name))
 			.filter((v) => v);
-		const pad = (s: any) => (s < 10 ? "0" : "") + s;
-		function formatTime(seconds: any): string {
+                const pad = (s: any) => (s < 10 ? "0" : "") + s;
+		const formatTime = (seconds: any) => {
 			const hours = Math.floor(seconds / (60 * 60));
 			const minutes = Math.floor((seconds % (60 * 60)) / 60);
 			const secs = Math.floor(seconds % 60);
 			return `${pad(hours)}:${pad(minutes)}:${pad(secs)}`;
-		}
-		const users = await this.client.DB.user.count();
-                const uban = await this.client.DB.user.count({ban: true});
-		function uptime() {
-			return newFunction(formatTime);
-		}
-		this.run = async (M: typings.ISimplifiedMessage): Promise<void> => {
-			const chitoge =
-				"https://c.tenor.com/veo9RwLpw8AAAAPo/nakano-yotsuba-wolverine.mp4";
-			return void this.client.sendMessage(
-				M.from,
-				{ url: chitoge },
-				MessageType.video,
-				{
-					quoted: M.WAMessage,
-					mimetype: Mimetype.gif,
-					caption: `━━━❰ 🅨︎🅞︎🅣︎🅢︎🅤︎🅑︎🅐︎ ❱━━━\n\n🔮 *Groups: ${
-				chats.length
-					}*\n\n🚦 *Uptime: ${uptime()}*\n\n🦆 *My cute people: ${users}*\n\n💢 *Banned Users: ${uban}*\n\n\n`,
-				}
-			);
 		};
-	}
-}
-
-function newFunction(formatTime: (seconds: any) => string) {
-	return formatTime(process.uptime());
+		const uptime = () => formatTime(process.uptime());
+		await M.reply(await request.buffer(`https://c.tenor.com/veo9RwLpw8AAAAPo/nakano-yotsuba-wolverine.mp4`),
+            MessageType.video,
+            Mimetype.gif,
+            undefined,
+            `━━━❰ 🅨︎🅞︎🅣︎🅢︎🅤︎🅑︎🅐︎ ❱━━━\n\n🔮 *Groups:  ${
+				this.client.chats.all().filter(chat => chat.jid.endsWith('g.us')).length}*\n\n🚦 *Uptime: ${uptime()}*\n\n🦆 *My cute people: ${users}*\n\n💢 *Banned Users: ${uban}*\n\n\n`,
+                    undefined
+                ).catch((reason: any) =>
+            M.reply(`✖ An error occurred. Please try again later.`))	
+	};
 }
