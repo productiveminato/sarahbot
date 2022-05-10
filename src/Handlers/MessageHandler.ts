316afbda4faf5c49ff8ea7c2985396f0e3022709
DexterGlitch <105044608+DexterGlitch@users.noreply.github.com>
2022-05-10 21:39:52 +0530

    Update MessageHandler.ts

--- src/Handlers/MessageHandler.ts
+++ src/Handlers/MessageHandler.ts
@@ -28,7 +28,7 @@ export default class MessageHandler {
 				this.client.user.name ||
 				this.client.user.vname ||
 				this.client.user.short ||
-				"Chitoge";
+				"Dexxy";
 		} else if (M.WAMessage.key.fromMe) return void null;
 		    
 		if (M.from.includes("status")) return void null;
