6b712f685a6b2ec4829f77aa729287e219849fc4
minato1220 <100998375+minato1220@users.noreply.github.com>
2022-05-18 13:42:17 +0530

    Create thanks.ts

---
+++ src/commands/General/thanks.ts
@@ -0,0 +1,34 @@
+** @format */
+
+import { MessageType, Mimetype } from "@adiwajshing/baileys";
+import MessageHandler from "../../Handlers/MessageHandler";
+import BaseCommand from "../../lib/BaseCommand";
+import WAClient from "../../lib/WAClient";
+import { ISimplifiedMessage } from "../../typings";
+
+export default class Command extends BaseCommand {
+	constructor(client: WAClient, handler: MessageHandler) {
+		super(client, handler, {
+			command: "thanks",
+			description: "Generally used to check if bot is Up",
+			category: "general",
+			usage: `${client.config.prefix}hi`,
+			baseXp: 10,
+		});
+	}
+
+	run = async (M: ISimplifiedMessage): Promise<void> => {
+		const chitoge =
+			"https://c.tenor.com/E6P9PZdh7W0AAAPo/date-a-live-kurumi.mp4";
+		return void this.client.sendMessage(
+			M.from,
+			{ url: chitoge },
+			MessageType.video,
+			{
+				quoted: M.WAMessage,
+				mimetype: Mimetype.gif,
+				caption: `You're Welcome 💖. \n`,
+			}
+		);
+	};
+}
